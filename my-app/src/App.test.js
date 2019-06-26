15fb5b095c7f94c5a602a4299b0a9614788490f6
migglespinosa <miguelespinosaiv@gmail.com>
2019-06-26 19:37:12 -0400

    Added Jest and Enzyme

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { configure, shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() });
-/*
+
+it('renders without crashing 2', () => {
+  shallow(<App />);
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-*/
+
 
 describe('<App/>', () => {
 
