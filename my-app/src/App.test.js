e519e7d6e70b6ae9cea6cab5ffd9a3606e53bc05
migglespinosa <miguelespinosaiv@gmail.com>
2019-06-25 21:36:47 -0400

    Before switching branches

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+/*
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+*/
 
 describe('<App/>', () => {
 
