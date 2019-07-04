132f8490bbdf65d571daa4381efe0091d214340f
migglespinosa <miguelespinosaiv@gmail.com>
2019-07-03 23:20:12 -0400

    Developed the Calculate component

--- my-app/src/Calculate/Calculate.js
+++ my-app/src/Calculate/Calculate.js
@@ -2,11 +2,54 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 
+function CalcButton(props){
+  const CourseRating = props.CourseRating;
+  const Score = props.Score;
+  const Slope = props.Slope;
+  console.log("CR " + CourseRating);
+  console.log("Score " + Score);
+  console.log("Slope " + Slope);
+  return(
+    <button type="button"
+            key="differential"
+            onClick={e => props.calculateDiff(Score,CourseRating,Slope)}>Calculate</button>
+  );
+}
+
 class Calculate extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      differential: 0
+    }
+    this.calculateDiff = this.calculateDiff.bind(this);
+  }
+
+  calculateDiff(Score, CourseRating, Slope){
+    const differential = (Score - CourseRating)*(113/Slope)
+    console.log(differential)
+    this.setState({
+      display: differential
+    });
+  }
+
   render(){
     return(
       <div>
-        <h1>This is the Calculate Component</h1>
+        <h1>Calculate your handicap differential.</h1>
+        <form>
+          Your Score: <input type="number" id="Score" name="Score"/><br/>
+          Course Rating: <input type="number" id="CourseRating "name="CourseRating"/><br/>
+          Slope Rating: <input type="number" id="Slope" name="Slope"/><br/>
+        </form><br/>
+          <CalcButton Score={8}
+                         CourseRating={2}
+                         Slope={4}
+                         calculateDiff={this.calculateDiff}/>
+
+        <h2>Your differential is: {this.state.differential}</h2>
+
       </div>
     )
   }
