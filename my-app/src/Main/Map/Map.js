5ec3e94b9279a6629531a8339e3991bf4c2fa91c
migglespinosa <miguelespinosaiv@gmail.com>
2019-08-04 20:04:52 -0400

    Added google-map api

--- my-app/src/Main/Map/Map.js
+++ my-app/src/Main/Map/Map.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import '../../App.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+
 
 class Map extends React.Component {
   render(){
     return(
       <div>
         <h1>This is the Map Component</h1>
+        <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyBduZ-iDIEaq4AI6MrEoJb6_OV_9WXM1y8 "
+        //{...other props}
+      >
+        <GoogleMap
+          id='example-map'
+          {...other props }
+        >
+          ...Your map components
+        </GoogleMap>
+      </LoadScript>
       </div>
     )
   }
