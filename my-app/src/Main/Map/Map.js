d11af7387a21f97a11b7967a23215565f387c6d9
migglespinosa <miguelespinosaiv@gmail.com>
2019-08-05 20:28:14 -0400

    Added the ability to get current location in <Map> using Geolocation API

--- my-app/src/Main/Map/Map.js
+++ my-app/src/Main/Map/Map.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import Demo from './Demo.js';
 import '../../App.css';
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 
 class Map extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.getInnerRef = this.getInnerRef.bind(this);
+      this.getLocation = this.getLocation.bind(this);
+  }
+
+  innerRef;
+  getInnerRef(ref) {
+      this.innerRef = ref;
+  }
+
+  getLocation() {
+      this.innerRef && this.innerRef.getLocation();
+  }
+
   render(){
+    const { getInnerRef, getLocation } = this;
     return(
       <div>
+        <article style={{ textAlign: "center" }}>
+        {/* eslint-disable-next-line no-console*/}
+          <Demo onError={error => console.log(error)} ref={getInnerRef} />
+          <button
+            className="pure-button pure-button-primary"
+            onClick={getLocation}
+            type="button"
+          >
+            Get location
+            </button>
+        </article>
+
         <h1>This is the Map Component</h1>
         <LoadScript
         id="script-loader"
         googleMapsApiKey="AIzaSyBduZ-iDIEaq4AI6MrEoJb6_OV_9WXM1y8"
-        //{...other props}
       >
         <GoogleMap
           id='example-map'
@@ -28,7 +57,7 @@ class Map extends React.Component {
         >
           ...Your map components
         </GoogleMap>
-      </LoadScript>
+        </LoadScript>
       </div>
     )
   }
