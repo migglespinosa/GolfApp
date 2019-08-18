8828542188d7439fe55e3e1a07a3cc1419f27058
migglespinosa <miguelespinosaiv@gmail.com>
2019-08-17 21:51:09 -0400

    Map works

--- my-app/src/Main/Map/Demo.js
+++ my-app/src/Main/Map/Demo.js
@@ -5,41 +5,12 @@ import { StandaloneSearchBox } from '@react-google-maps/api';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 class Demo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    //logErrorToMyService(error, errorInfo);
-    //console.log("Error :" + error);
-    //console.log("errorInfo :" + errorInfo)
-  }
-
-
-    render(){
-      if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return <h1>{this.props.coords.latitude}</h1>;
-      }
-      else{
-
-        return(
-
-          !this.props.isGeolocationAvailable ? (
+    render() {
+        return !this.props.isGeolocationAvailable ? (
             <div>Your browser does not support Geolocation</div>
         ) : !this.props.isGeolocationEnabled ? (
             <div>Geolocation is not enabled</div>
         ) : this.props.coords ? (
-          <div>
             <table>
                 <tbody>
                     <tr>
@@ -50,65 +21,42 @@ class Demo extends React.Component {
                         <td>longitude</td>
                         <td>{this.props.coords.longitude}</td>
                     </tr>
+                    <tr>
+                        <td>altitude</td>
+                        <td>{this.props.coords.altitude}</td>
+                    </tr>
+                    <tr>
+                        <td>heading</td>
+                        <td>{this.props.coords.heading}</td>
+                    </tr>
+                    <tr>
+                        <td>speed</td>
+                        <td>{this.props.coords.speed}</td>
+                    </tr>
                 </tbody>
-              </table>
-                /*
                 <LoadScript
                 id="script-loader"
                 googleMapsApiKey="AIzaSyBduZ-iDIEaq4AI6MrEoJb6_OV_9WXM1y8"
-              >
-                <GoogleMap
-                  id='example-map'
-                  mapContainerStyle={{
-                    height: "400px",
-                    width: "800px"
-                  }}
-                  zoom={14}
-                  center={{
-                    lat: this.props.coords.latitude,
-                    lng: this.props.coords.longitude
-                  }}
                 >
-
-                  <StandaloneSearchBox
-                    onError={error => console.log(error)}
-                    onLoad={ref => this.searchBox = ref}
-                    onPlacesChanged={
-                      () => console.log(this.searchBox.getPlaces())
-                    }
+                  <GoogleMap
+                    id='example-map'
+                    mapContainerStyle={{
+                      height: "400px",
+                      width: "800px"
+                    }}
+                    zoom={14}
+                    center={{
+                      lat: this.props.coords.latitude,
+                      lng: this.props.coords.longitude
+                    }}
                   >
-                    <input
-                      type="text"
-                      placeholder="Customized your placeholder"
-                      style={{
-                        boxSizing: `border-box`,
-                        border: `1px solid transparent`,
-                        width: `240px`,
-                        height: `32px`,
-                        padding: `0 12px`,
-                        borderRadius: `3px`,
-                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                        fontSize: `14px`,
-                        outline: `none`,
-                        textOverflow: `ellipses`,
-                        position: "absolute",
-                        left: "50%",
-                        marginLeft: "-120px"
-                      }}
-                    />
-                    </StandaloneSearchBox>
-
-                </GoogleMap>
+                  </GoogleMap>
                 </LoadScript>
-                */
-              </div>
-
+            </table>
         ) : (
-            <div>Getting the location data</div>
-        )
-      );
+            <div>Getting the location data&hellip; </div>
+        );
     }
-  }
 }
 
 export default geolocated({
