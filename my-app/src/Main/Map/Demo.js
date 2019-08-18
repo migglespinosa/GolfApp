7c1666229d454c44c201c7fa09967220b065e1f9
migglespinosa <miguelespinosaiv@gmail.com>
2019-08-17 20:38:26 -0400

    Before I attempt to revert back to a working <Map/> component

--- my-app/src/Main/Map/Demo.js
+++ my-app/src/Main/Map/Demo.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import Map from "./Map";
 import { geolocated } from "react-geolocated";
+import { StandaloneSearchBox } from '@react-google-maps/api';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 class Demo extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    //logErrorToMyService(error, errorInfo);
+    //console.log("Error :" + error);
+    //console.log("errorInfo :" + errorInfo)
+  }
+
+
+    render(){
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <h1>{this.props.coords.latitude}</h1>;
+      }
+      else{
+
         return(
 
           !this.props.isGeolocationAvailable ? (
@@ -24,6 +52,7 @@ class Demo extends React.Component {
                     </tr>
                 </tbody>
               </table>
+                /*
                 <LoadScript
                 id="script-loader"
                 googleMapsApiKey="AIzaSyBduZ-iDIEaq4AI6MrEoJb6_OV_9WXM1y8"
@@ -40,15 +69,46 @@ class Demo extends React.Component {
                     lng: this.props.coords.longitude
                   }}
                 >
-                  ...Your map components
+
+                  <StandaloneSearchBox
+                    onError={error => console.log(error)}
+                    onLoad={ref => this.searchBox = ref}
+                    onPlacesChanged={
+                      () => console.log(this.searchBox.getPlaces())
+                    }
+                  >
+                    <input
+                      type="text"
+                      placeholder="Customized your placeholder"
+                      style={{
+                        boxSizing: `border-box`,
+                        border: `1px solid transparent`,
+                        width: `240px`,
+                        height: `32px`,
+                        padding: `0 12px`,
+                        borderRadius: `3px`,
+                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                        fontSize: `14px`,
+                        outline: `none`,
+                        textOverflow: `ellipses`,
+                        position: "absolute",
+                        left: "50%",
+                        marginLeft: "-120px"
+                      }}
+                    />
+                    </StandaloneSearchBox>
+
                 </GoogleMap>
                 </LoadScript>
+                */
               </div>
+
         ) : (
             <div>Getting the location data</div>
         )
       );
     }
+  }
 }
 
 export default geolocated({
