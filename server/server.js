6c84973d3066fee8bff18a947f86bd99c93558e1
Developer <developer@miguels-air.middlebury.edu>
2020-02-23 15:58:31 -0500

    Changed start-strcipts in package.json

--- server/server.js
+++ server/server.js
@@ -21,10 +21,10 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 );
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join('../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/../', 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
   });
 }
 
