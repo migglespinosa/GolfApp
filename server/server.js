f6457fd00555d060dbc4fea67c9f6f266e310a89
Developer <developer@Miguels-MacBook-Air.local>
2020-03-21 20:20:09 -0400

    Made it better

--- server/server.js
+++ server/server.js
@@ -23,10 +23,11 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join('../client/build')));
-
+  /*
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
   });
+  */
 }
 
 const connection = mongoose.connection;
